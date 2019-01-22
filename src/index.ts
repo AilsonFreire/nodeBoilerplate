da91f76240e63c52a613b1bb317791283a3d1c20
Ailson Freire <ailson_freire@outlook.com>
2019-01-22 16:30:09 -0300

    configurando nodemon e tslint

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there' })
-})
+app.get("/", (req, res) => {
+    res.send({ hi: "there" });
+});
+
+const PORT = process.env.PORT;
 
 app.listen(5000);
