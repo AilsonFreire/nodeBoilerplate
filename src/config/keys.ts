d0ce5394ce23ecff6ca406e74da0e288e948b2ca
Ailson <ailson_freire@outlook.com>
2019-01-31 23:55:25 -0300

    Configurando google strategy

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
    googleClientID: "961511711102-k2jdmnac0codrdl5ctdibikeg2dsg90p.apps.googleusercontent.com",
    googleClientSecret: "0km0ZLB2fzIMKtkDBJgKXpgr"
 };
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,22 @@
 import express from "express";
 import passport from "passport";
-import GoogleStrategy from "passport-google-oauth2";
-
+import { Strategy as GoogleStrategy } from 'passport-google-oauth2';
+import Keys from "./config/keys";
 
 const app = express();
 
-// passport.use(new GoogleStrategy());
+passport.use(new GoogleStrategy({
+    clientID: Keys.googleClientID,
+    clientSecret: Keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken: string) => {
+    console.log(accessToken)
+}));
+
+app.get('/auth/google', passport.authenticate('google', {
+        scope: ['profile, email']
+}));
 
 const PORT: number = parseInt(<string>process.env.PORT) || 5000;
 
-app.listen(PORT, "0.0.0.0", () => console.log("asdf")  );
+app.listen(PORT, () => console.log("asdf"));
