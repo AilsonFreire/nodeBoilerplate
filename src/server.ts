cea0a61399136dc1fd2ae36dd96828f4c483ba84
Ailson <ailson_freire@outlook.com>
2019-02-10 23:54:10 -0200

    Ajustando auteticacao do google para o novo formato

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import passport from "passport";
-import { Strategy as GoogleStrategy } from 'passport-google-oauth2';
+import { Strategy as GoogleStrategy, VerifyCallback } from 'passport-google-oauth2'; // <-- IMPORT VERIFY CALLBACK TO USE AS TYPE FROM DONE
 import Keys from "./config/keys";
 
 const app = express();
@@ -8,14 +8,19 @@ const app = express();
 passport.use(new GoogleStrategy({
     clientID: Keys.googleClientID,
     clientSecret: Keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-}, (accessToken: string) => {
-    console.log(accessToken)
+    callbackURL: '/auth/google/callback',
+    userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo' // <--- ADD THIS LINE AFTER ADDING IN INTERFACE OF STRATEGY
+}, (accessToken: string, refreshToken: string, profile: any, done: VerifyCallback) => {
+    console.log('acessToken', accessToken)
+    console.log('refreshToken', refreshToken)
+    console.log('Profile', profile)
 }));
 
 app.get('/auth/google', passport.authenticate('google', {
-        scope: ['profile, email']
+    scope: ['profile', 'email']
 }));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT: number = parseInt(<string>process.env.PORT) || 5000;
 
